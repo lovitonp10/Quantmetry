a02a008d30138313e1bc8cd8095fafe1b466ad53
Gsaes <gsaes@quantmetry.com>
2023-04-28 16:52:23 +0200

    Ajout de metrics

--- dl4tsf/domain/metrics.py
+++ dl4tsf/domain/metrics.py
@@ -3,29 +3,81 @@ from evaluate import load
 from gluonts.time_feature import get_seasonality
 
 
-def estimate_mase_smape(cfg, forecasts, test_dataset):
-    forecast_median = np.median(forecasts, 1)
+def estimate_mae(cfg, forecasts, true_ts):
+    pred_length = cfg.model.model_config.prediction_length
+    for idx, (forecast, ts) in enumerate(zip(forecasts, true_ts)):
+        true_value = np.array(ts[-cfg.model.model_config.prediction_length :][idx])
+        foracast_value = np.array(forecast.median(axis=1))
+        mae_metrics = (np.sum(np.abs(true_value - foracast_value))) / pred_length
+    return mae_metrics
+
+
+def estimate_rmse(cfg, forecasts, true_ts):
+    pred_length = cfg.model.model_config.prediction_length
+    for idx, (forecast, ts) in enumerate(zip(forecasts, true_ts)):
+        true_value = np.array(ts[-cfg.model.model_config.prediction_length :][idx])
+        forecast_value = np.array(forecast.median(axis=1))
+        rmse_metrics = (np.sum((true_value - forecast_value) ** 2) / pred_length) ** (0.5)
+    return rmse_metrics
+
+
+def estimate_mape(cfg, forecasts, true_ts):
+    pred_length = cfg.model.model_config.prediction_length
+    for idx, (forecast, ts) in enumerate(zip(forecasts, true_ts)):
+        true_value = np.array(ts[-cfg.model.model_config.prediction_length :][idx])
+        forecast_value = np.array(forecast.median(axis=1))
+        mape_metrics = (100 / pred_length) * np.sum(
+            np.abs((true_value - forecast_value) / true_value)
+        )
+    return mape_metrics
+
+
+def estimate_smape(cfg, forecasts, true_ts):
+    pred_length = cfg.model.model_config.prediction_length
+    for idx, (forecast, ts) in enumerate(zip(forecasts, true_ts)):
+        true_value = np.array(ts[-cfg.model.model_config.prediction_length :][idx])
+        forecast_value = np.array(forecast.median(axis=1))
+        smape_metrics = (200 / pred_length) * np.sum(
+            np.abs(true_value - forecast_value) / (np.abs(true_value) + np.abs(forecast_value))
+        )
+    return smape_metrics
+
+
+def estimate_wmape(cfg, forecasts, true_ts):
+    for idx, (forecast, ts) in enumerate(zip(forecasts, true_ts)):
+        true_value = np.array(ts[-cfg.model.model_config.prediction_length :][idx])
+        forecast_value = np.array(forecast.median(axis=1))
+        wmape_metrics = (
+            100 * np.sum(np.abs(true_value - forecast_value)) / np.sum(np.abs(true_value))
+        )
+    return wmape_metrics
+
+
+def estimate_mase(cfg, forecasts, true_ts):
     mase_metric = load("evaluate-metric/mase")
-    smape_metric = load("evaluate-metric/smape")
-    mase_metrics = []
-    smape_metrics = []
-    i = 0
-    for item_id, ts in enumerate(test_dataset):
-        print(i)
-        training_data = ts["target"][: -cfg.model.model_config["prediction_length"]]
-        ground_truth = ts["target"][-cfg.model.model_config["prediction_length"] :]
+    for idx, (forecast, ts) in enumerate(zip(forecasts, true_ts)):
+        true_value = np.array(ts[-cfg.model.model_config.prediction_length :][idx])
+        forecast_value = np.array(forecast.median(axis=1))
         mase = mase_metric.compute(
-            predictions=forecast_median[item_id],
-            references=np.array(ground_truth),
-            training=np.array(training_data),
+            predictions=forecast_value,
+            references=true_value,
+            training=np.array(ts[idx]),
             periodicity=get_seasonality(cfg.dataset.freq),
         )
-        mase_metrics.append(mase["mase"])
+        mase_metrics = mase["mase"]
+    return mase_metrics
 
-        smape = smape_metric.compute(
-            predictions=forecast_median[item_id],
-            references=np.array(ground_truth),
+
+def estimate_mase2(cfg, forecasts, true_ts):
+    mae = estimate_mae(cfg=cfg, forecasts=forecasts, true_ts=true_ts)
+    pred_length = cfg.model.model_config.prediction_length
+    for idx, (forecast, ts) in enumerate(zip(forecasts, true_ts)):
+        freq = get_seasonality(cfg.dataset.freq)
+        true_value = np.array(ts[-cfg.model.model_config.prediction_length :][idx])
+        true_value_seasonal = np.array(ts[-cfg.model.model_config.prediction_length - freq :][idx])
+
+        mae_naive = (
+            1 / (pred_length - freq) * np.sum(true_value[freq:] - true_value_seasonal[-freq:])
         )
-        smape_metrics.append(smape["smape"])
-        i = i + 1
-    return smape_metrics, mase_metrics
+        mase2_metrics = mae / mae_naive
+    return mase2_metrics
