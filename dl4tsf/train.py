bdbc5458639fb9001e2366a1af408596ebf94f17
Ubuntu <ubuntu@ip-172-31-43-35.eu-west-3.compute.internal>
2023-08-08 09:05:46 +0000

    modif on train.py

--- dl4tsf/train.py
+++ dl4tsf/train.py
@@ -21,6 +21,8 @@ logger.info("Start")
 
 @hydra.main(version_base="1.3", config_path="configs", config_name="config")
 def main(cfgHydra: DictConfig):
+    torch.cuda.empty_cache
+
     if torch.cuda.is_available():
         print("CUDA is available on this device.")
     else:
@@ -75,18 +77,17 @@ def main(cfgHydra: DictConfig):
 
     logger.info("Compute Prediction")
     # ts_it, forecast_it = forecaster.predict(test_dataset=dataset.validation, validation=True)
-    metrics, ts_it, forecast_it = forecaster.evaluate(test_dataset=dataset.validation)
-    logger.info(ts_it[0].tail())
-    logger.info(forecast_it[0].head())
+    metrics, ts_it, forecast_it = forecaster.evaluate(test_dataset=dataset.test)
 
-    logging_mlflow.log_plots(
-        item_id=0,
-        ts_it=ts_it,
-        forecast_it=forecast_it,
-        map_item_id=loader_data.get_map_item_id(),
-        nb_past_pts=cfg.model.model_config.prediction_length * 10,
-        validation=True,
-    )
+    for i in range(10):
+        logging_mlflow.log_plots(
+            item_id=i,
+            ts_it=ts_it,
+            forecast_it=forecast_it,
+            map_item_id=loader_data.get_map_item_id(),
+            nb_past_pts=cfg.model.model_config.prediction_length * 10,
+            validation=True,
+        )
 
     mlflow.log_metrics(get_mean_metrics(metrics))
     # logger.info(metrics)
